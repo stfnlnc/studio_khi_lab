ea208a54ddd8dda5dbf340110333eec9106c18a4
Stefan <stefan@studiokhi.com>
2024-08-30 16:37:37 +0200

    feat: responsive design

--- resources/main/js/footer-lab.js
+++ resources/main/js/footer-lab.js
@@ -1,121 +1,165 @@
 import gsap from 'gsap';
 import {ScrollTrigger} from "gsap/ScrollTrigger";
-import { ScrollSmoother } from 'gsap/ScrollSmoother';
+import {ScrollSmoother} from 'gsap/ScrollSmoother';
+
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-let smoother = ScrollSmoother.create({
-    wrapper: '#smooth-wrapper',
-    content: '#smooth-content',
-    smooth: 2,
-});
+window.onresize = function () {
+    location.reload();
+}
 
-gsap.set('.title', {top: '50%', left: '50%', xPercent: -50, yPercent: -50})
-gsap.set('.btn-container', {top: '50%', left: '50%', xPercent: -50, yPercent: -50, zIndex: 0})
-gsap.to('.btn-container', {
-    y: -250,
-    duration: 1.2,
-    scrollTrigger: {
-        start: '50% 50%',
-        end: '300% 50%',
-        trigger: '.title',
-        scrub: 1,
-        markers: false
-    }
-})
+window.onload = function () {
 
-gsap.to('.title', {
-    y: -200,
-    duration: 1.2,
-    scrollTrigger: {
-        start: '50% 50%',
-        end: '300% 50%',
-        trigger: '.title',
-        scrub: 2,
-        markers: false
-    }
-})
+    let smoother = ScrollSmoother.create({
+        wrapper: '#smooth-wrapper',
+        content: '#smooth-content',
+        smooth: 2,
+    });
 
-gsap.to('.container', {
-    duration: 1,
-    scrollTrigger: {
-        start: '50% 50%',
-        end: '150% 50%',
-        trigger: '.container',
-        markers: false,
-        pin: true,
-    }
-})
+    gsap.set('.title', {top: '60%', left: '50%', xPercent: -50, yPercent: -50})
+    gsap.set('.btn-container', {top: '60%', left: '50%', xPercent: -50, yPercent: -50, zIndex: 0})
+    gsap.to('.btn-container', {
+        y: -250,
+        duration: 1.2,
+        ease: 'power1.inOut',
+        scrollTrigger: {
+            start: '50% 50%',
+            end: '300% 50%',
+            trigger: '.title',
+            scrub: 1,
+            markers: false
+        }
+    })
 
-gsap.to('.cloud-2', {
-    y: -150,
-    duration: 1.2,
-    scrollTrigger: {
-        start: 'top 50%',
-        end: '50% 50%',
-        trigger: '.cloud-1',
-        scrub: 2,
-        markers: false
-    }
-})
+    gsap.to('.title', {
+        y: -200,
+        duration: 1.2,
+        ease: 'power1.inOut',
+        scrollTrigger: {
+            start: '50% 50%',
+            end: '300% 50%',
+            trigger: '.title',
+            scrub: 2,
+            markers: false
+        }
+    })
 
-gsap.to('.cloud-3', {
-    y: -300,
-    duration: 1.2,
-    scrollTrigger: {
-        start: 'top 50%',
-        end: '50% 50%',
-        trigger: '.cloud-1',
-        scrub: 2,
-        markers: false
-    }
-})
+    gsap.to('.container', {
+        duration: 1,
+        scrollTrigger: {
+            start: '50% 50%',
+            end: '150% 50%',
+            trigger: '.container',
+            markers: false,
+            pin: true,
+        }
+    })
 
-gsap.to('body', {
-    backgroundColor: "#0F0E0E",
-    duration: 0.8,
-    scrollTrigger: {
-        start: 'top 50%',
-        end: 'bottom 50%',
-        toggleActions: 'play reverse play reverse',
-        trigger: '.cloud-3',
-        markers: false,
-    }
-})
-gsap.to('.btn-top', {
-    backgroundColor: "#0F0E0E",
-    borderColor: "#0F0E0E",
-    color: "#0F0E0E",
-    duration: 0.8,
-    scrollTrigger: {
-        start: 'top 50%',
-        end: 'bottom 50%',
-        toggleActions: 'play reverse play reverse',
-        trigger: '.cloud-3',
-        markers: false,
-    }
-})
+    gsap.to('.cloud-2', {
+        y: -150,
+        duration: 1.2,
+        scrollTrigger: {
+            start: 'top 50%',
+            end: '50% 50%',
+            trigger: '.cloud-1',
+            scrub: 2,
+            markers: false
+        }
+    })
 
-const navBtn = document.querySelector('.nav-btn')
-const navItems = document.querySelectorAll('.nav-item')
+    gsap.to('.cloud-3', {
+        y: -300,
+        duration: 1.2,
+        scrollTrigger: {
+            start: 'top 50%',
+            end: '50% 50%',
+            trigger: '.cloud-1',
+            scrub: 2,
+            markers: false
+        }
+    })
 
-console.log(navItems[1].getBoundingClientRect().left)
+    gsap.to('body', {
+        backgroundColor: "#0F0E0E",
+        duration: 0.8,
+        scrollTrigger: {
+            start: 'top 50%',
+            end: 'bottom 50%',
+            toggleActions: 'play reverse play reverse',
+            trigger: '.cloud-2',
+            markers: false,
+        }
+    })
+    gsap.to('.btn-top', {
+        backgroundColor: "#0F0E0E",
+        borderColor: "#0F0E0E",
+        color: "#0F0E0E",
+        duration: 0.8,
+        scrollTrigger: {
+            start: 'top 50%',
+            end: 'bottom 50%',
+            toggleActions: 'play reverse play reverse',
+            trigger: '.cloud-2',
+            markers: false,
+        }
+    })
 
-gsap.set(navBtn, {
-    top: navItems[0].getBoundingClientRect().top,
-    left: navItems[0].getBoundingClientRect().left,
-    width: navItems[0].getBoundingClientRect().width,
-    height: navItems[0].getBoundingClientRect().height,
-})
+    const navBtn = document.querySelector('.nav-btn')
+    const navItems = document.querySelectorAll('.nav-item')
 
-navItems.forEach((item, key) => {
-    item.addEventListener('click', () => {
-        gsap.to(navBtn, {
-            top: navItems[key].getBoundingClientRect().top,
-            left: navItems[key].getBoundingClientRect().left,
-            width: navItems[key].getBoundingClientRect().width,
-            height: navItems[key].getBoundingClientRect().height,
-            duration: 0.6,
-            ease: "power4.inOut"
+    gsap.set(navBtn, {
+        top: navItems[0].getBoundingClientRect().top,
+        left: navItems[0].getBoundingClientRect().left,
+        width: navItems[0].getBoundingClientRect().width,
+        height: navItems[0].getBoundingClientRect().height,
+    })
+
+    gsap.fromTo(navBtn, {opacity: 0}, {
+        opacity: 1,
+        duration: 0
+    })
+
+    navItems.forEach((item, key) => {
+        item.addEventListener('click', () => {
+            gsap.to(navBtn, {
+                top: navItems[key].getBoundingClientRect().top,
+                left: navItems[key].getBoundingClientRect().left,
+                width: navItems[key].getBoundingClientRect().width,
+                height: navItems[key].getBoundingClientRect().height,
+                duration: 0.6,
+                ease: "power4.inOut"
+            })
         })
     })
-})
+
+    const navMenu = document.querySelector('.nav-menu')
+    const nav = document.querySelector('.nav')
+
+
+    navMenu.addEventListener('click', () => {
+        let heightNav = ''
+        if (navMenu.classList.contains('open')) {
+            heightNav = 'auto'
+            navMenu.classList.remove('open')
+            navMenu.classList.add('close')
+        } else if (navMenu.classList.contains('close')) {
+            heightNav = '2.745rem'
+            navMenu.classList.remove('close')
+            navMenu.classList.add('open')
+        }
+        gsap.to(nav, {
+            height: heightNav
+        })
+    })
+
+
+    navItems.forEach(item => {
+        item.addEventListener('click', () => {
+            navItems.forEach(item => {
+                item.classList.remove('nav-item-active')
+            })
+            item.classList.add('nav-item-active')
+        })
+    })
+
+}
--- resources/views/main/templates/footer/index.blade.php
+++ resources/views/main/templates/footer/index.blade.php
@@ -7,8 +7,11 @@
 @section('title', 'Footer')
 
 @section('content')
-    <div class="nav-btn"></div>
-    <div class="nav flex flex-row gap-2.5 relative">
+    <div class="nav-btn sm:block hidden"></div>
+    <div class="nav flex sm:flex-row flex-col gap-2.5 relative overflow-hidden">
+        <div class="nav-menu sm:hidden open">
+            Menu
+        </div>
         <a href="#" class="nav-item nav-item-active">
             Home
         </a>
@@ -30,7 +33,7 @@
             <section class="flex flex-col items-center justify-center relative">
                 <div class="relative container flex flex-row items-center justify-center">
                     <h1 class="title absolute w-full">You like <br> our work ?</h1>
-                    <div class="flex flex-row gap-6 btn-container absolute">
+                    <div class="flex sm:flex-row flex-col sm:gap-6 gap-2 btn-container absolute">
                         <div class="btn btn-top">Our services</div>
                         <div class="btn btn-light btn-top">Our projects</div>
                     </div>
@@ -41,19 +44,19 @@
                     <img class="absolute cloud-3" src="{{ asset('/images/templates/footer/cloud-3.png') }}" alt="">
                     <div class="p-5 footer-container absolute w-full flex flex-row items-start justify-between">
                         <h1 class="h1-light text-left">Say <br> hello.</h1>
-                        <div class="footer-item flex flex-row gap-24 mr-40">
-                            <div class="flex flex-row gap-6 footer-btn-container absolute">
-                                <div class="btn">Book your call</div>
-                                <div class="btn btn-light">Email us</div>
-                            </div>
-                            <div class="flex flex-col gap-6">
+                        <div class="flex flex-row sm:gap-6 gap-2 footer-btn-container absolute">
+                            <div class="btn">Book your call</div>
+                            <div class="btn btn-light">Email us</div>
+                        </div>
+                        <div class="footer-item flex flex-row lg:gap-24 sm:gap-12 gap-8 sm:pl-20 pl-5 mr-20">
+                            <div class="flex flex-col lg:gap-6 sm:gap-4 gap-2 hidden-mobile">
                                 <a href="#">Home</a>
                                 <a href="#">Services</a>
                                 <a href="#">Pricing</a>
                                 <a href="#">About</a>
                                 <a href="#">Contact</a>
                             </div>
-                            <div class="flex flex-col gap-6">
+                            <div class="flex flex-col lg:gap-6 sm:gap-4 gap-2">
                                 <a href="#">Instagram</a>
                                 <a href="#">Twitter</a>
                                 <a href="#">Linkedin</a>
