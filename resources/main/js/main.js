c0b6d92720635f4d04d7de528c8460b7c69cbc41
Stefan <stefan@studiokhi.com>
2024-10-01 18:00:46 +0200

    feat: footer appearance

--- resources/main/js/main.js
+++ resources/main/js/main.js
@@ -1,42 +1,137 @@
+import {gsap} from "gsap";
+import {ScrollSmoother} from 'gsap/ScrollSmoother';
+import {ScrollTrigger} from 'gsap/ScrollTrigger';
+import {SplitText} from 'gsap/SplitText';
 
-const random = document.querySelector('#random');
+gsap.registerPlugin(ScrollSmoother, ScrollTrigger, SplitText);
+
+ScrollSmoother.create({
+    wrapper: '#smooth-wrapper',
+    content: '#smooth-content',
+    smooth: 2,
+});
+
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+}
+
 const preloader = document.querySelector('#preloader');
-let logos = document.querySelectorAll('.logo');
+const logos = document.querySelectorAll('.logo');
+const body = document.querySelector('body')
+body.style.overflow = 'hidden'
 
 const colors = [
-'#DAD6EA',
-'#CDDFE6',
-'#D3E0D3',
-'#EAE0D6'
+    '#DAD6EA',
+    '#CDDFE6',
+    '#D3E0D3',
+    '#EAE0D6',
+    '#F1F1F1'
 ];
 
-function generateRandomNumber(item) {
-    let lastNumber = null;
-    let counter = 0;
+function generateRandomSvg() {
     const maxIterations = 10;
     const generatedNumbers = new Set();
+    let counter = 0;
+    let lastColor = '';
+
     const intervalId = setInterval(() => {
         if (counter >= maxIterations) {
             clearInterval(intervalId);
-            preloader.classList.remove('bg-dark')
-            let newColorNumber;
-            preloader.style.backgroundColor = colors[Math.floor(Math.random() * (colors.length))];
+            preloader.classList.remove('bg-dark');
+            lastColor = colors[Math.floor(Math.random() * colors.length)];
+            preloader.style.backgroundColor = lastColor;
             logos.forEach(logo => logo.style.filter = 'invert(100)');
-            return;
+            setTimeout(() => { body.style.overflow = 'auto'; }, 600);
+            return lastColor;
         }
+
         let newNumber;
         do {
-            newNumber = Math.floor(Math.random() * (logos.length));
+            newNumber = Math.floor(Math.random() * logos.length);
         } while (generatedNumbers.has(newNumber));
         generatedNumbers.add(newNumber);
 
-        lastNumber = newNumber;
         logos.forEach(logo => logo.style.display = 'none');
         logos[newNumber].style.display = 'block';
         counter++;
     }, 180);
+
+    return lastColor;
 }
+
 setTimeout(() => {
-    generateRandomNumber(random);
+    generateRandomSvg();
 }, 500)
 
+const templates = document.querySelectorAll('.template');
+let i = 0;
+templates.forEach(template => {
+    ScrollTrigger.create({
+        trigger: template,
+        markers: false,
+        start: "top 80%",
+        end: "bottom 0%",
+        scrub: true,
+        onEnter: () => {
+            gsap.to(preloader, {duration: 0.1, ease: 'power1.inOut', backgroundColor: colors[i]})
+            if (i === colors.length - 1) {
+                i = 0
+            } else {
+                i++;
+            }
+        },
+        onLeaveBack: () => {
+            gsap.to(preloader, {duration: 0.1, ease: 'power1.inOut', backgroundColor: colors[i]})
+            if (i === 0) {
+                i = colors.length - 1
+            } else {
+                i--;
+            }
+        },
+    })
+})
+
+const reveals = document.querySelectorAll('.reveal');
+
+reveals.forEach(reveal => {
+    gsap.fromTo(reveal, {
+        yPercent:100
+    }, {
+        yPercent:0,
+        delay: 3.5,
+        duration: 1.6,
+        ease: 'power2.inOut'
+    })
+})
+
+const revealDown = document.querySelector('.reveal-down')
+
+gsap.fromTo(revealDown, {
+    yPercent:-100,
+}, {
+    yPercent:0,
+    delay: 3.5,
+    duration: 1.6,
+    ease: 'power2.inOut'
+})
+
+
+function pinItemToLastSection(item) {
+    const lastSection = document.querySelector('#last-section')
+    gsap.to(item, {
+        y: '-100vh',
+        duration: 3,
+        ease: 'power1.inOut',
+        scrollTrigger: {
+            start: 'top top',
+            end: 'bottom top',
+            trigger: lastSection,
+            markers: false,
+            scrub: true
+        }
+    })
+}
+
+pinItemToLastSection(preloader)
+pinItemToLastSection('#scroll-down')
+pinItemToLastSection('header')
--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -5,13 +5,86 @@
 @endsection
 
 @section('content')
-
-    <section id="preloader" class="fixed top-0 left-0 w-screen h-screen bg-dark transition-all duration-[1800ms]">
+    <header
+        class="fixed top-0 left-0 w-full flex flex-row justify-between text-base z-30 overused px-5 py-2.5 text-dark mix-blend-difference invert">
+        <div class="overflow-hidden">
+            <div class="reveal">
+                <a target="_blank" class="" href="https://studiokhi.com">Digital Exploration by Studio Khi</a>
+            </div>
+        </div>
+        <div class="overflow-hidden absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <div class="reveal">
+                <div class="flex flex-row gap-2">
+                    <a href="" class=" hover:opacity-100 transition-all duration-300">Home,</a>
+                    <a href="" class="opacity-50 hover:opacity-100 transition-all duration-300">Index,</a>
+                    <a href="" class="opacity-50 hover:opacity-100 transition-all duration-300">About</a>
+                </div>
+            </div>
+        </div>
+        <div class="overflow-hidden">
+            <div class="reveal">
+                <div class="border-b border-dark">
+                    <a class="reveal" href="">Send us a message</a>
+                </div>
+            </div>
+        </div>
+    </header>
+    <section id="preloader" class="fixed top-0 left-0 w-screen h-screen bg-dark transition-colors duration-[1800ms] z-0">
         <div class="flex items-center justify-center h-full pb-12">
-            <img class="logo xl:w-3/4 w-full" src="{{ asset('images/logo/22.svg') }}" alt="">
+            <img class="logo w-full" src="{{ asset('images/logo/22.svg') }}" alt="">
             @for($i = 1; $i < 21; $i++)
-                <img class="hidden logo transition-all duration-1000 xl:w-3/4 w-full" id="{{ $i }}" src="{{ asset('images/logo/') . '/' . $i . '.svg' }}" alt="">
+                <img class="hidden logo transition-all duration-1000 w-full" id="{{ $i }}"
+                     src="{{ asset('images/logo/') . '/' . $i . '.svg' }}" alt="">
             @endfor
         </div>
     </section>
+    <div id="scroll-down" class="overflow-hidden fixed bottom-2.5 left-1/2 -translate-x-1/2 mix-blend-difference invert z-20">
+        <div
+            class="flex flex-col items-center justify-center gap-2 py-6 overused text-dark reveal-down">
+            <div>Scroll Down</div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                <path
+                    d="M10 17.5L16.25 11.25L15.3688 10.3688L10.625 15.1063V2.5H9.375V15.1063L4.63125 10.3688L3.75 11.25L10 17.5Z"
+                    fill="#191919"/>
+            </svg>
+        </div>
+    </div>
+    <div id="smooth-wrapper">
+        <div id="smooth-content">
+            <section class="w-full h-screen flex flex-col items-center justify-center">
+
+            </section>
+            <section class="relative template w-full h-[calc(100vh-5rem)] flex flex-col items-center justify-center">
+                <img class="bg-amber-100 translate-x-96"
+                     src="https://studiokhi.com/uploads/projects/featured/full/moon-opticien-lunetier-ea5121c24ef0b62bbbcea9f764e7fda6.png"
+                     alt=""/>
+            </section>
+            <section class="template w-full h-[calc(100vh-5rem)] flex flex-col items-center justify-center">
+                <img class="bg-amber-100 -translate-x-96"
+                     src="https://studiokhi.com/uploads/projects/featured/full/moon-opticien-lunetier-ea5121c24ef0b62bbbcea9f764e7fda6.png"
+                     alt=""/>
+            </section>
+            <section id="last-section" class="w-full h-screen flex flex-col items-center justify-center">
+
+            </section>
+            <section class="relative w-full h-screen flex flex-col items-center justify-center bg-transparent">
+
+            </section>
+        </div>
+    </div>
+    <footer class="fixed top-0 left-0 bg-dark w-full h-screen pt-48 px-5 -z-10">
+        <div class="w-full flex flex-row items-start justify-between text-base text-grey">
+            <div><span class="opacity-50">Developed by</span> <span class="underline">Stefan Lancelot</span></div>
+            <div class="text-center">
+                Digital exploration by <a class="underline" href="https://studiokhi.com">Studio Khi</a>. <br>
+                A French studio that creates websites and designs <br>
+                for bold brands and entrepreneurs who want to stand out on the web.
+            </div>
+            <div><span class="opacity-50">Designed by</span> <span class="underline">Cynthia Jego</span></div>
+        </div>
+        <div class="absolute w-full bottom-0 left-0 flex flex-row items-end justify-center">
+                <img class="w-full" src="{{ asset('images/logo/') . '/' . rand(1, 20) . '.svg' }}" alt="">
+        </div>
+    </footer>
+
 @endsection
